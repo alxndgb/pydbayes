675d30f000306516428a10759c96e86789d32466
Paul McDonagh <mcdonaghp@alxn.com>
2015-06-25 10:39:09 -0400

    Fixed and standardized code formatting issues

--- target/dist/pydbayes-1.0.dev0/build/lib/utilities.py
+++ target/dist/pydbayes-1.0.dev0/build/lib/utilities.py
@@ -1,16 +1,14 @@
-def readDataFromFile(filename, sep = '\t'):
-	dataFile = open(filename, 'r')
-	columnHeadings = dataFile.readline()
-	columnHeadings = columnHeadings.strip('\n')
-	columns = columnHeadings.split(sep)
-	data = [[] for dummy in xrange(len(columns))]
-	for line in dataFile.readlines():
-		fields = line.strip('\n').split(sep)
-		for i, number in enumerate(fields):
-			data[i].append(number)
-	results = list()
-	results.append(columns)
-	results.append(data)
-	return results
-
-
+def readDataFromFile(filename, sep='\t'):
+    dataFile = open(filename, 'r')
+    columnHeadings = dataFile.readline()
+    columnHeadings = columnHeadings.strip('\n')
+    columns = columnHeadings.split(sep)
+    data = [[] for dummy in xrange(len(columns))]
+    for line in dataFile.readlines():
+        fields = line.strip('\n').split(sep)
+        for i, number in enumerate(fields):
+            data[i].append(number)
+    results = list()
+    results.append(columns)
+    results.append(data)
+    return results
--- target/dist/pydbayes-1.0.dev0/build/scripts-2.7/driver.py
+++ target/dist/pydbayes-1.0.dev0/build/scripts-2.7/driver.py
@@ -1,32 +1,31 @@
 #############################################################################
 # There is an R-like data frame package
 import pandas as pan
-import numpy as np
-import math
 
-dataframe = pan.read_table('smallDF_pheno.txt', sep = '\t')
-#data = dataframe.drop(dataframe.columns[0], axis = 1)
+import pydbayes
+
+dataframe = pan.read_table('smallDF_pheno.txt', sep='\t')
+# data = dataframe.drop(dataframe.columns[0], axis = 1)
 data = dataframe
-data = data * 1.0 # Convert to floats - will be easier later on
+data = data * 1.0  # Convert to floats - will be easier later on
 dimensions = data.shape
 print(dimensions)
 
 globalEQSS = 550
 genotypes = data[data.columns[1]]
 clinical = data[data.columns[2]]
-conditionalPrior = generateGeneticPrior(genotypes, clinical, globalEQSS)
-score = MCR(data, conditionalPrior, 1, 2)
+conditionalPrior = pydbayes.scoring.generateGeneticPrior(genotypes, clinical, globalEQSS)
+score = pydbayes.scoring.MCR(data, conditionalPrior, 1, 2)
 print(score)
 
 
 # Work out some timings
-with Timer() as t:
-	for test in xrange(0,1000):
-		score = MCR(data, conditionalPrior, 1, 2)
+with pydbayes.Timer() as t:
+    for test in xrange(0, 1000):
+        score = pydbayes.scoring.MCR(data, conditionalPrior, 1, 2)
 print(t.secs)
 
-
-with Timer() as t:
-	for test in xrange(0,1000):
-		score = MCRFast(data, conditionalPrior, 1, 2, 3, 4)
+with pydbayes.Timer() as t:
+    for test in xrange(0, 1000):
+        score = pydbayes.scoring.MCRFast(data, conditionalPrior, 1, 2, 3, 4)
 print(t.secs)
